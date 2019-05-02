1ef02d65f1d50313671cf12ec2174607e244e806
tuipik <tuipiku@gmail.com>
2019-05-02 14:53:11 +0300

    added timing

--- e_dictionary.py
+++ e_dictionary.py
@@ -14,22 +14,25 @@ def main():
     description = '\n- '+'\n- '.join(description)
     print(word, description)
     root = tk.Tk()
-    root.geometry('{}x{}+{}+{}'.format(610, 400, 1200,150))
+    root.geometry('{}x{}+{}+{}'.format(610, 400, position_y, position_x))
     ttk.Style().configure("TLabel", padding=12, relief="flat",
                           wraplength=600)
-    text1 = ttk.Label(root, font=('Arial 14 bold'), text= word)
+    text1 = ttk.Label(root, font=('Arial 20 bold'), text= word)
     text2 = ttk.Label(root, font=('Arial 12'), text= description)
     text1.pack()
     text2.pack()
-    root.after(7000, lambda: root.destroy())
+    root.after(8000, lambda: root.destroy())
     root.mainloop()
 
-
+sleep_sec = random.randint(20, 25)
+position_x = random.randint(50, 700)
+position_y = random.randint(50, 1200)
 
 
 
 if __name__ == '__main__':
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, "dictionary.json")
-
-    main()
+    for i in range(1):
+        # time.sleep(sleep_sec)
+        main()
